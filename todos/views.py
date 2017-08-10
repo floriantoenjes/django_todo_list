c6956e190f3f1d7e01ce97d3e335d8b1ab05a85d
Florian Tönjes <skatepainter@yahoo.de>
2017-08-10 17:38:41 +0200

    Using get_object_or_404 method now

--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Item, TodoList
 
@@ -8,9 +8,9 @@ def todo_list_overview(request):
     return render(request, "todos/todo_list_overview.html", {"todo_lists": todo_lists})
 
 def todo_list_detail(request, todo_list_pk):
-    todo_list = TodoList.objects.get(pk=todo_list_pk)
+    todo_list = get_object_or_404(TodoList, pk=todo_list_pk)
     return render(request, "todos/todo_list_detail.html", {"todo_list": todo_list})
 
 def item_detail(request, todo_list_pk, item_pk):
-    item = Item.objects.get(todo_list_id=todo_list_pk, pk=item_pk)
+    item = get_object_or_404(Item, todo_list_id=todo_list_pk, pk=item_pk)
     return render(request, "todos/item_detail.html", {"item": item})
