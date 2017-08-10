43054c49e3cf7f8a0400cdde04c3e741d7f5825c
Florian Tönjes <skatepainter@yahoo.de>
2017-08-10 14:51:14 +0200

    Todo list detail view implemented

--- todos/urls.py
+++ todos/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r"^$", views.todo_list_overview, name="todo_list_overview"),
+    url(r"^(?P<todo_list_pk>\d+)/$", views.todo_list_detail, name="todo_list_detail")
 ]
--- todos/views.py
+++ todos/views.py
@@ -6,3 +6,7 @@ from .models import Item, TodoList
 def todo_list_overview(request):
     todo_lists = TodoList.objects.all()
     return render(request, "todos/todo_list_overview.html", {"todo_lists": todo_lists})
+
+def todo_list_detail(request, todo_list_pk):
+    todo_list = TodoList.objects.get(pk=todo_list_pk)
+    return render(request, "todos/todo_list_detail.html", {"todo_list": todo_list})
