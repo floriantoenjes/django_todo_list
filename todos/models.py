4e0b1b3645c0f8bec704d6a78b891001bdf92b92
Florian Tönjes <skatepainter@yahoo.de>
2017-08-10 14:10:05 +0200

    Initial commit

---
+++ todos/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class TodoList(models.Model):
+    name = models.CharField(max_length=60)
+    created_at = models.DateTimeField(auto_now_add=True)
+    order = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ["order",]
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField(default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+    order = models.IntegerField(default=0)
+    todoList = models.ForeignKey(TodoList)
+
+    class Meta:
+        ordering = ["order",]
+
+    def __str__(self):
+        return self.name
