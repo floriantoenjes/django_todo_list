3ffdbd64e65e447b686266b1339d3bee92dc3fe7
Florian Tönjes <skatepainter@yahoo.de>
2017-08-12 20:02:43 +0200

    Added a filter for completed state of items

--- todos/admin.py
+++ todos/admin.py
@@ -4,8 +4,10 @@ from .models import TodoList, Item
 
 
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ["name", "order", "completed"]
-    list_editable = ["order", "completed"]
+    list_filter = ["completed"]
+    list_display = ["name", "todo_list", "order", "completed"]
+    list_editable = ["order", "todo_list", "completed"]
+    search_fields = ["name", "description"]
 
 admin.site.register(TodoList)
 admin.site.register(Item, ItemAdmin)
