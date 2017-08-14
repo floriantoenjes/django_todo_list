cda1ad1ac601c72c6554541f5110f3a6cdb23fa7
Florian Tönjes <skatepainter@yahoo.de>
2017-08-14 19:46:42 +0200

    Edited todo list detail template to show some links only if the user is authenticated

--- todos/urls.py
+++ todos/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     url(r"^$", views.TodoListListView.as_view(), name="todo_list_overview"),
     url(r"^(?P<pk>\d+)/$", views.TodoListDetailView.as_view(), name="todo_list"),
     url(r"^(?P<todo_list_pk>\d+)/edit_todo_list/$", views.todo_list_edit, name="edit_todo_list"),
-    url(r"^(?P<pk>\d+)/delete_todo_list/$", views.TodoListDeleteView.as_view(), name="delete_todo_list"),
+    url(r"^(?P<pk>\d+)/delete_todo_list/$", views.TodoListDeleteView.as_view(), name="delete_todo_list")
+    ,
     url(r"^(?P<todo_list_pk>\d+)/create_item/$", views.item_create, name="create_item"),
     url(r"^(?P<todo_list_pk>\d+)/(?P<item_pk>\d+)/$", views.item_detail, name="item"),
     url(r"^(?P<todo_list_pk>\d+)/edit_item/(?P<item_pk>\d+)/$", views.item_edit, name="edit_item"),
