7762fc053b2a04699844b0ba8f80b79639d6fe2c
Florian Tönjes <skatepainter@yahoo.de>
2017-08-15 16:55:59 +0200

    Added ListCreateItem view

--- todos/api_v1_urls.py
+++ todos/api_v1_urls.py
@@ -6,4 +6,5 @@ from . import api_v1_views
 urlpatterns = [
     url(r"^$", api_v1_views.ListCreateTodoList.as_view(), name="todo_list_overview"),
     url(r"^(?P<pk>\d+)/$", api_v1_views.RetrieveUpdateDestroyTodoList.as_view(), name="todo_list_detail"),
+    url(r"^(?P<todo_list_pk>\d+)/items/$", api_v1_views.ListCreateItem.as_view(), name="item_list"),
 ]
--- todos/api_v1_views.py
+++ todos/api_v1_views.py
@@ -1,7 +1,9 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 
 from . import models
 from . import serializers
+
 
 class ListCreateTodoList(generics.ListCreateAPIView):
     queryset = models.TodoList.objects.all()
@@ -10,3 +12,14 @@ class ListCreateTodoList(generics.ListCreateAPIView):
 class RetrieveUpdateDestroyTodoList(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.TodoList.objects.all()
     serializer_class = serializers.TodoListSerializer
+
+class ListCreateItem(generics.ListCreateAPIView):
+    queryset = models.Item.objects.all()
+    serializer_class = serializers.ItemSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(todo_list_id=self.kwargs.get("todo_list_pk"))
+
+    def perform_create(self, serializer):
+        todo_list = get_object_or_404(models.TodoList, pk=self.kwargs.get("todo_list_pk"))
+        serializer.save(todo_list=todo_list)
