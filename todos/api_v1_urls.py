ef89cd0b49d77ca1f58babbff191c8eb0bb2ad4a
Florian Tönjes <skatepainter@yahoo.de>
2017-08-15 12:22:14 +0200

    Added RetrieveUpdateDestroyTodoList view

--- todos/api_v1_urls.py
+++ todos/api_v1_urls.py
@@ -5,4 +5,5 @@ from . import api_v1_views
 
 urlpatterns = [
     url(r"^$", api_v1_views.ListCreateTodoList.as_view(), name="todo_list_overview"),
+    url(r"^(?P<pk>\d+)/$", api_v1_views.RetrieveUpdateDestroyTodoList.as_view(), name="todo_list_detail"),
 ]
--- todos/api_v1_views.py
+++ todos/api_v1_views.py
@@ -6,3 +6,7 @@ from . import serializers
 class ListCreateTodoList(generics.ListCreateAPIView):
     queryset = models.TodoList.objects.all()
     serializer_class = serializers.TodoListSerializer
+
+class RetrieveUpdateDestroyTodoList(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.TodoList.objects.all()
+    serializer_class = serializers.TodoListSerializer
