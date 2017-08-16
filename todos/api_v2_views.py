166500a2624348f600a2626f6992933b6b64475c
Florian Tönjes <skatepainter@yahoo.de>
2017-08-16 19:55:38 +0200

    Added detail route for items

--- todos/api_v2_views.py
+++ todos/api_v2_views.py
@@ -1,8 +1,17 @@
 from rest_framework import viewsets
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
 
 from todos import models, serializers
+from todos.models import Item
 
 
 class TodoListViewSet(viewsets.ModelViewSet):
     queryset = models.TodoList.objects.all()
     serializer_class = serializers.TodoListSerializer
+
+    @detail_route(methods=["get"])
+    def items(self, request, pk=None):
+        items = Item.objects.filter(todo_list_id=pk)
+        serializer = serializers.ItemSerializer(items, many=True)
+        return Response(serializer.data)
